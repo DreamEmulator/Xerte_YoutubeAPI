ee0d30646b1a96f8cc2c8f1942e0aea4140fbeea
Sebastiaan Hols <shg.hols@gmail.com>
2018-11-07 14:47:20 +0100

    Enjoy 🤓

---
+++ Xerte_YouTubeAPI.js
@@ -0,0 +1,86 @@
+/////////////////////////// Op de specifieke Xerte pagina - SCRIPT: ///////////////////////////
+var settings = {
+    videoId : 'rAt1vxwQG9c',
+    startSeconds: 0,
+    endSeconds: 0,
+    mute: 0,
+}
+AddYouTubeVideo(settings);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+///////////////////////////Op de start pagina van de Xerte Module - SCRIPT://///////////////////////
+$.getScript('https://coo.erasmusmc.nl/xerte/js/xerte_erasmus.js');
+
+function LoadYoutubeAPI() {
+    console.log('Loading Youtube API...');
+    $.getScript("https://www.youtube.com/iframe_api");
+}
+
+function LoadPlayer(settings) {
+
+    var player;
+
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: settings.videoId,
+        playerVars: {
+            start: settings.startSeconds,
+            end: settings.endSeconds,
+            mute: settings.mute,
+            showInfo: 0,
+            controls: 0,
+            rel: 0,
+            iv_load_policy: 3,
+            playsinline: 1
+        },
+        events: {
+            onStateChange: onPlayerStateChange
+        }
+    });
+
+//Voor het opnieuw laden van de video als deze afgelopen is
+    function onPlayerStateChange(event) {
+        if (event.data === 0) {
+            player.loadVideoById({
+                videoId: settings.videoId,
+                startSeconds: settings.startSeconds,
+                endSeconds: settings.endSeconds
+            });
+        }
+    }
+
+    $('.iframe-cover').click(function () {
+        player.playVideo();
+        setTimeout(function () {
+            $('.iframe-cover').remove();
+        }, 500);
+    });
+
+
+}
+
+function AddYouTubeVideo(settings) {
+
+        try {
+            LoadPlayer(settings);
+            console.log("Youtube Succesfully Loaded");
+            this.break;
+        } catch (err) {
+            console.log("Youtube not loaded yet: Trying again...");
+            LoadYoutubeAPI();
+            setTimeout(function () {
+                AddYouTubeVideo(settings)
+            }, 100);
+        }
+
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
