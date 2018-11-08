4328deec6e3568f282450de4f515aa6462f82ebd
sebastiaan <shg.hols@gmail.com>
2018-11-08 14:50:59 +0100

    Bug fixes

--- Xerte_YouTubeAPI.js
+++ Xerte_YouTubeAPI.js
@@ -45,7 +45,7 @@ function LoadPlayer(settings) {
 //Voor het opnieuw laden van de video als deze afgelopen is
     function onPlayerStateChange(event) {
         if (event.data === -1) {
-            $('.iframe-cover').addClass('show');
+            $('.iframe-cover').removeClass('show');
         }
 
         if (event.data === 0) {
@@ -54,16 +54,21 @@ function LoadPlayer(settings) {
                 startSeconds: settings.startSeconds,
                 endSeconds: settings.endSeconds
             });
+            $('.iframe-cover').removeClass('show');
+            player.pauseVideo()
+        }
+
+        if (event.data === 1) {
+            $('.iframe-cover').addClass('show');
         }
 
         if (event.data === 2) {
-            $('.iframe-cover').addClass('show');
+            $('.iframe-cover').removeClass('show');
         }
     }
 
     $('.iframe-cover').click(function () {
         player.playVideo();
-        $('.iframe-cover').addClass('show');
     });
 
 
